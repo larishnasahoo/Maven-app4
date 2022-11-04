9dfa90789cdd1f84f92ba6b7d74c5cd3a4ba63b2
larishnasahoo <larishnasahoo@gmail.com>
2022-11-04 10:34:16 +0530

    Hello WOrld

---
+++ src/main/java/mavenapp3/mavenapp/MavenappApplication.java
@@ -0,0 +1,13 @@
+package mavenapp3.mavenapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mavenapp3/mavenapp/MavenappApplicationTests.java
@@ -0,0 +1,13 @@
+package mavenapp3.mavenapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
